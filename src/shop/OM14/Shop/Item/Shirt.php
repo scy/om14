aef05b565716a535bcfa070a8d8c217e5c6d89bf
Tim Weber <scy@scy.name>
2014-08-10 10:41:38 +0200

    Add note about shirts not being mailed

--- src/shop/OM14/Shop/Item/Shirt.php
+++ src/shop/OM14/Shop/Item/Shirt.php
@@ -8,7 +8,7 @@ class Shirt extends Item {
 
 	protected static $type = 'SHIRT';
 	protected static $title = 'T-Shirt';
-	protected static $description = 'Baumwolle, kleidsames Schwarz, selbstverständlich mit #om14-Logo. Achtung: nur bis 20.08. 12:00 Uhr mittags bestellbar!';
+	protected static $description = 'Baumwolle, kleidsames Schwarz, selbstverständlich mit #om14-Logo. Achtung: nur bis 20.08. 12:00 Uhr mittags bestellbar! Kein Versand, müssen auf der #om14 abgeholt werden!';
 	protected static $maxTime = 1408528800; // 2014-08-20 12:00:00 CEST
 	protected static $minPrice = 25;
 
